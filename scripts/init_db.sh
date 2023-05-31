509932cc181fb7daef671e43d8b4b1421dc5349d
kshitijmohan15 <92985242+kshitijmohan15@users.noreply.github.com>
2023-05-31 14:09:22 +0530

    fixed database not found error, local db and docker db were mapped to the same port, hence error

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -20,7 +20,7 @@ fi
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=2345}"
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]; then
     docker run \
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,4 @@
 // this test is framework and language proof. If tomorrow i decided that I want to ditch Rust and go back to NodeJS, I can use this same test suite to keep track of my API endpoints, we would just have to change the way we spawn the app, example: change the bash script that must run before the tests begin.
-
 use std::net::TcpListener;
 
 #[tokio::test]
